36d84573259157e8dfc7bfaf5b63cc9e4d1cc2a3
DTounda <toundanana@gmail.com>
2026-06-23 19:38:47 +0200

    feat: add export to CSV script

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,16 +6,25 @@ import sys
 import urllib.request
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: {} <user_id>".format(sys.argv[0]))
+        sys.exit(1)
+
     user_id = int(sys.argv[1])
     base_url = "https://jsonplaceholder.typicode.com"
+
     user_url = "{}/users/{}".format(base_url, user_id)
     with urllib.request.urlopen(user_url) as response:
         user = json.loads(response.read().decode())
+
     username = user.get("username")
+
     todos_url = "{}/todos?userId={}".format(base_url, user_id)
     with urllib.request.urlopen(todos_url) as response:
         todos_list = json.loads(response.read().decode())
+
     filename = "{}.csv".format(user_id)
+
     with open(filename, "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos_list:
