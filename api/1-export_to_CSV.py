9e7043bac0c45b9042371bad936d1882ddd6e51b
DTounda <toundanana@gmail.com>
2026-06-23 19:51:09 +0200

    fix: use string user_id, remove lineterminator for PEP8

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
-"""Exports employee TODO list data to CSV format."""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
-import json
+import requests
 import sys
-import urllib.request
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    user_url = "{}/users/{}".format(base_url, user_id)
-    with urllib.request.urlopen(user_url) as response:
-        user = json.loads(response.read().decode())
-
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    todos_url = "{}/todos?userId={}".format(base_url, user_id)
-    with urllib.request.urlopen(todos_url) as response:
-        todos_list = json.loads(response.read().decode())
-
-    filename = "{}.csv".format(user_id)
-
-    with open(filename, "w", newline="") as csvfile:
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todos_list:
+        for task in todos:
             writer.writerow([
                 user_id,
                 username,
