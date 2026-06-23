d44cefe40006494436cf9ca943357fa6eaa096fe
DTounda <toundanana@gmail.com>
2026-06-23 19:16:07 +0200

    feat: add export to CSV script

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Exports employee TODO list data to CSV format."""
+import csv
+import json
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    user_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = "{}/users/{}".format(base_url, user_id)
+    with urllib.request.urlopen(user_url) as response:
+        user = json.loads(response.read().decode())
+    username = user.get("username")
+    todos_url = "{}/todos?userId={}".format(base_url, user_id)
+    with urllib.request.urlopen(todos_url) as response:
+        todos_list = json.loads(response.read().decode())
+    filename = "{}.csv".format(user_id)
+    with open(filename, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos_list:
+            writer.writerow([
+                user_id,
+                username,
+                task.get("completed"),
+                task.get("title")
+            ])
